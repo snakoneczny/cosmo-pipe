cac03f331c4ed32dbd786af7846ee3ef61ebca5d
Szymon Nakoneczny <szymon.nakoneczny@gmail.com>
2021-01-09 14:55:55 +0100

    notebooks directory

--- env_config.py
+++ env_config.py
@@ -1,4 +1,4 @@
 # DATA_PATH = '/mnt/home/snakoneczny/data'
-DATA_PATH = '/Users/snakoneczny/data'
+DATA_PATH = '/home/snakoneczny/data'
 # PROJECT_PATH = '/mnt/home/snakoneczny/workspace/cmb_lss'
-PROJECT_PATH = '/Users/snakoneczny/workspace/cmb_lss'
+PROJECT_PATH = '/home/snakoneczny/workspace/cmb_lss'
