mcmc covariance fix

experiment.py
@@ -324,9 +324,8 @@ class Experiment:
                 a_end = a_start + n_ells_a
                 b_end = b_start + n_ells_b
 
-                # TODO: make sure the order is right, fix last indexing
                 self.inference_covariance[a_start: a_end, b_start: b_end] = \
-                    self.covariance_matrices[self.config.error_method][corr_symbol_b + '-' + corr_symbol_a][
+                    self.covariance_matrices[self.config.error_method][corr_symbol_a + '-' + corr_symbol_b][
                     bin_range_a[0]:bin_range_a[1], bin_range_b[0]:bin_range_b[1]]
 
                 b_start += n_ells_b
@@ -334,7 +333,7 @@ class Experiment:
 
         if self.config.fit_tomographer:
             n_tomo = len(self.tomographer_z_arr)
-            np.fill_diagonal(self.inference_covariance[-n_tomo:, -n_tomo:], tomographer_n_err_arr)
+            np.fill_diagonal(self.inference_covariance[-n_tomo:, -n_tomo:], tomographer_n_err_arr ** 2)
 
         self.inference_correlation = get_correlation_matrix(self.inference_covariance)
         self.inverted_covariance = np.linalg.inv(self.inference_covariance)

mcmc_report.py
@@ -48,11 +48,11 @@ def show_mcmc_report(experiment_name, data_name, burnin=None, thin=None):
     make_param_plots(config, labels, samples)
 
     # Tau plot
-    plot_mean_tau(tau_arr)
+    print('Mean acceptance fraction: {}'.format(np.mean(emcee_sampler.acceptance_fraction)))
+    # print('Number of iterations: {}'.format(len(tau_arr)))
     print('burn-in: {0}'.format(burnin))
     print('thin: {0}'.format(thin))
-    print('Mean acceptance fraction: {}'.format(np.mean(emcee_sampler.acceptance_fraction)))
-    print('Number of iterations: {}'.format(len(tau_arr)))
+    plot_mean_tau(tau_arr)
 
     # Samples history
     plot_samples_history(labels, samples, log_prob_samples)